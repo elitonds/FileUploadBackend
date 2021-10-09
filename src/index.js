4f8507d75cda8a8c83b08bf6cc232466a4d157ee
elitonds <elitonds@gmail.com>
2021-10-08 23:15:35 -0300

    finalizando upload de arquivo via s3 e local

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
+require('dotenv').config();
+
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
+const env = require('dotenv');
 
-//const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, SERVER_PORT } = process.env;
+const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, SERVER_PORT } = process.env;
 const app = express();
 
 /**
  * Database setup
  */
-mongoose.connect(`mongodb://user:user@0.0.0.0:27017/upload?authSource=admin`,{
-    useNewUrlParser: true
+mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`, {
+  useNewUrlParser: true,
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 
 app.use(require("./routes"));
 
-app.listen(3000);
+app.listen(SERVER_PORT);
